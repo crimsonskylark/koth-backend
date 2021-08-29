26eac0ce154918b47258b68489f9a86ce9632e7d
crimsonskylark <47747084+crimsonskylark@users.noreply.github.com>
2021-08-29 17:08:28 -0300

    - refactor InfantryModel to use diffferent settings

--- Server/Map/TeamZone.cs
+++ Server/Map/TeamZone.cs
@@ -7,7 +7,7 @@ namespace koth_server.Map
     {
         public string Name { get; set; }
         public float[] SafeZone { get; set; }
-        public string InfantryModel { get; set; }
+        public List<int[]> Uniform { get; set; }
         public float[] PlayerSpawnCoords { get; set; }
         public List<float[]> HeliSpawnPoints { get; set; }
         public List<float[]> CarSpawnPoints { get; set; }
